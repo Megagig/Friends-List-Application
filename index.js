b5b389107050fbd314b13841ada07e4cf73d0a5a
Megagig <megagigsoftwaresolutions.com>
2024-11-07 10:58:32 +0100

    Connected to the database

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
+const mongoose = require('mongoose');
 const registerRouter = require("./routes/register");
 require('dotenv').config();
 const app = express();
 
 //middleware to parse JSON request
 app.use(express.json());
+
+//middleware to handle session
+
+//connect to the mongodb database
+mongoose.connect(process.env.MONGODB_URI, {}).then(() => {
+	console.log('Connected to the database');
+}).catch((err) => {
+	console.log('Error connecting to the database', err);
+});
 
 //routes
 app.use('/api/v1/register', registerRouter);
